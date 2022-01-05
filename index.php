8df5a4a24a511aba90292f8b2491faa1977dfdf8
Bocustt <88473761+Bocustt@users.noreply.github.com>
2022-01-05 03:04:17 -0800

    tampil data

--- index.php
+++ index.php
@@ -1,3 +1,154 @@
+<?php
+    //koneksi Database
+    $host = "db4free.net";
+    $user = "bocust";
+    $pass ="bukasandi";
+    $database = "dbmaba";
+
+    $koneksi = mysqli_connect($host, $user, $pass, $database) or die (mysqli_eror($koneksi));
+
+    //jika tombol simpan diklik
+    if(isset($_POST['bsimpan']))
+    {
+        //Pengujian apakah data akan diedit atau disimpan baru
+        if($_GET['hal'] == "edit")
+        {
+            //Data akan diedit
+            $edit = mysqli_query($koneksi, "UPDATE maba set
+                                                no_pendaftaran = '$_POST[tno_pendaftaran]',
+                                                nama = '$_POST[tnama]',                                               
+                                                nik = '$_POST[tnik]',
+                                                nisn = '$_POST[tnisn]',
+                                                ttl = '$_POST[tttl]',
+                                                alamat = '$_POST[talamat]',
+                                                jeniskelamin = '$_POST[tjeniskelamin]',
+                                                tahun_lulus = '$_POST[ttahun_lulus]',
+                                                sekolahasal = '$_POST[tsekolahasal]',
+                                                agama = '$_POST[tagama]',
+                                                negara = '$_POST[tnegara]',
+                                                nohp = '$_POST[tnohp]',
+                                                email = '$_POST[temail]',
+                                                nama_ayah = '$_POST[tnama_ayah]',
+                                                pk_ayah = '$_POST[tpk_ayah]',
+                                                nama_ibu = '$_POST[tnama_ibu]',
+                                                pk_ibu = '$_POST[tpk_ibu]',
+                                                penghasilan = '$_POST[tpenghasilan]',
+                                                jarak = '$_POST[tjarak]',
+                                                transport = '$_POST[ttransport]',
+                                                ttsk = '$_POST[tttsk]',
+                                                prodi = '$_POST[tprodi]'
+                                            WHERE id_pendaftaran = '$_GET[id]'
+                                        ");
+                                       
+            if($edit) //jika edit sukses
+            {
+                echo "<script>
+                        alert('edit data Sukses');
+                        document.location='index.php';
+                    </script>";
+            }
+            else
+            {
+                echo "<script>
+                        alert('edit data Gagal!!');
+                        document.location='index.php';
+                    </script>";
+            }
+        }else
+        {
+            //Data akan disimpan baru
+            $simpan = mysqli_query($koneksi, "INSERT INTO maba (no_pendaftaran, nama, nik, nisn, ttl, alamat, jeniskelamin, tahun_lulus, sekolahasal, agama, negara, nohp, email, nama_ayah, pk_ayah, nama_ibu, pk_ibu, penghasilan, jarak, transport, ttsk, prodi)
+                                              VALUES (  '$_POST[tno_pendaftaran]',
+                                                        '$_POST[tnama]',
+                                                        '$_POST[tnik]', 
+                                                        '$_POST[tnisn]',
+                                                        '$_POST[tttl]',
+                                                        '$_POST[talamat]',
+                                                        '$_POST[tjeniskelamin]',
+                                                        '$_POST[ttahun_lulus]',
+                                                        '$_POST[tsekolahasal]',
+                                                        '$_POST[tagama]',
+                                                        '$_POST[tnegara]',
+                                                        '$_POST[tnohp]',
+                                                        '$_POST[temail]',
+                                                        '$_POST[tnama_ayah]',
+                                                        '$_POST[tpk_ayah]',
+                                                        '$_POST[tnama_ibu]',
+                                                        '$_POST[tpk_ibu]',
+                                                        '$_POST[tpenghasilan]',
+                                                        '$_POST[tjarak]',
+                                                        '$_POST[ttransport]',
+                                                        '$_POST[tttsk]', 
+                                                        '$_POST[tprodi]')
+                                        ");
+            if($simpan) //jika simpan sukses
+            {
+                echo "<script>
+                        alert('Simpan data Sukses');
+                        document.location='index.php';
+                    </script>";
+            }
+            else
+            {
+                echo "<script>
+                        alert('Simpan data Gagal!!');
+                        document.location='index.php';
+                    </script>";
+            }
+        }
+    }
+
+
+
+        //pengujian jika tombol edit atau hapus di klik
+        if(isset($_GET['hal']))
+        {
+            //pengujian jike edit data
+            if($_GET["hal"] == "edit")
+            {
+                //tampil data yang akan diedit
+                $tampil = mysqli_query($koneksi, "SELECT * FROM maba WHERE id_pendaftaran = '$_GET[id]' ");
+                $data = mysqli_fetch_array($tampil);
+                if($data)
+                {
+                    //jika data ditemukan, maka data ditampung ke dalam variabel
+                    $vno_pendaftaran = $data['no_pendaftaran'];
+                    $vnama = $data['nama'];
+                    $vnik = $data['nik'];
+                    $vnisn = $data['nisn'];
+                    $vttl = $data['ttl'];
+                    $valamat = $data['alamat'];
+                    $vjeniskelamin = $data['jeniskelamin'];
+                    $vtahun_lulus = $data['tahun_lulus'];
+                    $vsekolahasal = $data['sekolahasal'];
+                    $vagama = $data['agama'];
+                    $vnegara = $data['negara'];
+                    $vnohp = $data['nohp'];
+                    $vemail = $data['email'];
+                    $vnama_ayah = $data['nama_ayah'];
+                    $vpk_ayah = $data['pk_ayah'];
+                    $vnama_ibu = $data['nama_ibu'];
+                    $vpk_ibu = $data['pk_ibu'];
+                    $vpenghasilan = $data['penghasilan'];
+                    $vjarak = $data['jarak'];
+                    $vtransport = $data['transport'];
+                    $vttsk = $data['ttsk'];
+                    $vprodi = $data['prodi'];
+                }
+            }
+            else if ($_GET ['hal'] == "hapus")
+            {
+                //Persiapan hapus data
+                $hapus = mysqli_query($koneksi, "DELETE FROM maba WHERE id_ pendaftaran = '$_GET[id]' ");
+                if($hapus){
+                    echo "<script>
+                        alert('Hapus data Sukses!!');
+                        document.location='index.php';
+                    </script>";
+                }
+            }
+        }
+?>
 
 <!DOCTYPE html>
 <html>
@@ -18,24 +169,28 @@
     </div>
     <div class="card-body">
         <form method="post" action="">
+        <div class="form-group">
+                <label>NO. PENDAFTARAN</label>
+                <input type="text" name="tno_pendaftaran" value="<?=@$vno_pendaftaran?>" class="form-control" placeholder="masukan nomor pendaftaran" required>          
+            </div>
             <div class="form-group">
                 <label>NAMA</label>
-                <input type="text" name="tnama" value="<?=@$vnama?>" class="form-control" placeholder="Input Nama Sesuai Akta Kelahiran" required>          
+                <input type="text" name="tnama" value="<?=@$vnama?>" class="form-control" placeholder="Masukan nama sesuai akta" required>          
             </div>
             <div class="form-group">
                 <label>NIK</label>
-                <input type="text" name="tnik" value="<?=@$vnik?>" class="form-control" placeholder="Input NIK Anda disini!" required>
+                 <input type="text" name="tnik" value="<?=@$vnik?>" class="form-control" placeholder="Masukan NIK Anda disini!" required>
             </div>
             <div class="form-group">
                 <label>NISN</label>
-                <input type="text" name="tnisn" value="<?=@$vnisn?>" class="form-control" placeholder="Input NISN Anda disini!" required>
+                <input type="text" name="tnisn" value="<?=@$vnisn?>" class="form-control" placeholder="Masukan NISN Anda disini!" required>
             </div>
             <div class="form-group">
                 <label>TTL</label>
-                <textarea class="form-control" name="tttl" placeholder="Input TTL Anda Disini!"><?=@$vttl?></textarea>
+                <textarea class="form-control" name="tttl" placeholder="Masukan TTL Anda Disini!"><?=@$vttl?></textarea>
             <div class="form-group">
                 <label>ALAMAT</label>
-                <textarea class="form-control" name="talamat" placeholder="Input Alamat Anda Disini!"><?=@$valamat?></textarea>
+                <textarea class="form-control" name="talamat" placeholder="Masukan Alamat Anda Disini!"><?=@$valamat?></textarea>
             </div>
            
             <div class="form-group">
@@ -48,11 +203,11 @@
             </div>
             <div class="form-group">
                 <label>TAHUN LULUS</label>
-                <textarea class="form-control" name="tthnlulus" placeholder="Input Tahun Lulus!"><?=@$vthnlulus?></textarea>
+                <textarea class="form-control" name="ttahun_lulus" placeholder="Masukan Tahun Lulus!"><?=@$vtahun_lulus?></textarea>
             </div>
             <div class="form-group">
                 <label>SEKOLAH ASAL (SMA)</label>
-                <textarea class="form-control" name="tasalsklh" placeholder="Input Asal Sekolah Anda Disini!"><?=@$vasalsklh?></textarea>
+                <textarea class="form-control" name="tsekolahasal" placeholder="Masukan Asal Sekolah Anda Disini!"><?=@$vsekolahasal?></textarea>
             </div>
           
             <div class="form-group">
@@ -76,27 +231,27 @@
    			</select>
             <div class="form-group">
                 <label>NO.HP</label>
-                <textarea class="form-control" name="tnohp" placeholder="Input No. Hp Anda Disini!"><?=@$vnohp?></textarea>
+                <textarea class="form-control" name="tnohp" placeholder="Masukan No. Hp Anda Disini!"><?=@$vnohp?></textarea>
             </div>
             <div class="form-group">
                 <label>EMAIL</label>
-                <textarea class="form-control" name="temail" placeholder="Input No. Hp Anda Disini!"><?=@$vemail?></textarea>
+                <textarea class="form-control" name="temail" placeholder="Masukan Email Anda Disini!"><?=@$vemail?></textarea>
             </div>
             <div class="form-group">
                 <label>NAMA AYAH</label>
-                <input type="text" name="tnama_ayah" value="<?=@$vnama_ayah?>" class="form-control" placeholder="Input Nama Sesuai Akta Kelahiran" required>          
+                <input type="text" name="tnama_ayah" value="<?=@$vnama_ayah?>" class="form-control" placeholder="Masukan Nama Sesuai Akta" required>          
             </div>
             <div class="form-group">
                 <label>PEKERJAAN AYAH</label>
-                <input type="text" name="tpk_ayah" value="<?=@$vpk_ayah?>" class="form-control" placeholder="Input Nama Sesuai Akta Kelahiran" required>          
+                <input type="text" name="tpk_ayah" value="<?=@$vpk_ayah?>" class="form-control" placeholder="Masukan Pekerjaan Ayah" required>          
             </div>
             <div class="form-group">
                 <label>NAMA IBU</label>
-                <input type="text" name="tnama_ibu" value="<?=@$vnama_ibu?>" class="form-control" placeholder="Input Nama Sesuai Akta Kelahiran" required>          
+                <input type="text" name="tnama_ibu" value="<?=@$vnama_ibu?>" class="form-control" placeholder="Masukan Nama Sesuai Akta" required>          
             </div>
             <div class="form-group">
                 <label>PEKERJAAN IBU</label>
-                <input type="text" name="tpk_ibu" value="<?=@$vpk_ibu?>" class="form-control" placeholder="Input Nama Sesuai Akta Kelahiran" required>          
+                <input type="text" name="tpk_ibu" value="<?=@$vpk_ibu?>" class="form-control" placeholder="Masukan Pekerjaan Ibu" required>          
             </div>
             <div class="form-group">
    			    <label>PENGHASILAN KOMULATIF PER BULAN</label>
@@ -159,6 +314,82 @@
     <!-- Akhir Card Form -->
 
 
+    <!-- Awal Card tabel -->
+    <div class="card mt-3">
+    <div class="card-header bg-success text-white">
+        Daftar Calon Mahasiswa Polnustar
+    </div>
+    <div class="card-body">
+        
+    <table class="table table-boordered table-striped">
+        <tr>
+            <th>NO</th>
+            <th>NO. PENDAFTARAN</th>
+            <th>NAMA</th>
+            <th>NIK</th>
+            <th>NISN</th>
+            <th>TTL</th>
+            <th>ALAMAT</th>
+            <th>JENIS KELAMIN</th>
+            <th>TAHUN LULUS</th>
+            <th>SEKOLAH ASAL</th>
+            <th>AGAMA</th>
+            <th>KENEGARAAN</th>
+            <th>NO. HP</th>
+            <th>EMAIL</th>
+            <th>NAMA AYAH</th>
+            <th>PEKERJAAN AYAH</th>
+            <th>NAMA IBU</th>
+            <th>PEKERJAAN IBU</th>
+            <th>PENGHASILAN</th>
+            <th>JARAK KE KAMPUS</th>
+            <th>TRANSPORTASI</th>
+            <th>TINGGAL</th>
+            <th>PRODI</th>
+            <th>Aksi</th>
+        </tr>
+          <?php
+            $no = 1;
+            $tampil = mysqli_query($koneksi, "SELECT * from maba order by id_pendaftaran desc");
+            while($data = mysqli_fetch_array($tampil)) : 
+
+         ?>
+        <tr>
+            <td><?=$no++;?></td>
+            <td><?=$data['no_pendaftaran']?></td>
+            <td><?=$data['nama']?></td>
+            <td><?=$data['nik']?></td>
+            <td><?=$data['nisn']?></td>
+            <td><?=$data['ttl']?></td>
+            <td><?=$data['alamat']?></td>
+            <td><?=$data['jeniskelamin']?></td>
+            <td><?=$data['tahun_lulus']?></td>
+            <td><?=$data['sekolahasal']?></td>
+            <td><?=$data['agama']?></td>
+            <td><?=$data['negara']?></td>
+            <td><?=$data['nohp']?></td>
+            <td><?=$data['email']?></td>
+            <td><?=$data['nama_ayah']?></td>
+            <td><?=$data['pk_ayah']?></td>
+            <td><?=$data['nama_ibu']?></td>
+            <td><?=$data['pk_ibu']?></td>
+            <td><?=$data['penghasilan']?></td>
+            <td><?=$data['jarak']?></td>
+            <td><?=$data['transport']?></td>
+            <td><?=$data['ttsk']?></td>
+            <td><?=$data['prodi']?></td>
+        <td>
+                <a href="index.php?hal=edit&id=<?=$data['id_pendaftaran']?>" class="btn btn-warning"> Edit </a>
+                <a href="index.php?hal=hapus&id=<?=$data['id_pendaftaran']?>" 
+                onclick="return confirm('Apakah yakin ingin menghapus data ini?')" class="btn btn-danger"> Hapus </a>
+            </td>
+        </tr>
+        <?php endwhile; //penutup perulangan while ?>
+    </table>
+
+    </div>
+    </div>
+    <!-- Akhir Card tabel -->
 </div>
 
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
